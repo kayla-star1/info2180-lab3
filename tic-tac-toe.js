a06568335c80f31bafc0ede3528161223b00c669
Kayla Swift <kayla.swift1302@gmail.com>
2022-10-12 21:30:55 -0500

    JS ex2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,24 @@
-onload = function (){
-    
-    myDiv = document.getElementById("board").children;
-     for (let i = 0; i < myDiv.length; i++){
-         myDiv[i].setAttribute("class","square")
-     }
-    console.log(myDiv);
+let myArray =[]
+   function handleClick(e){
+    if (myArray.length == 0 || myArray[0]=="O"){
+       e.textContent="X"
+       e.classList.add ("X")
+       myArray[0]= "X"
+    } else if (myArray[0]== "X"){
+        e.textContent="O"
+        e.classList.add ("O")
+        myArray[0]= "O"  
+    }
 }
-   
+window.onload = function (){
+        let myDiv = document.getElementById("board").children;
+     
+        for (let i = 0; i < myDiv.length; i++){
+             myDiv[i].setAttribute("class","square")
+             myDiv[i].setAttribute("onclick","handleClick(this)")
+         }
+
+        console.log(myDiv);
+    
+}
+
